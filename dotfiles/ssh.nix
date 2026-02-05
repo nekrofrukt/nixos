0c71a3a73ab219dcf1e61bd851e30822432b927c
nekrofrukt <nekrofrukt.pm.me>
2026-02-05 10:55:13 +0100

    Set up GNOME and dotfiles

--- dotfiles/ssh.nix
+++ dotfiles/ssh.nix
@@ -4,10 +4,19 @@
   programs.ssh = {
     enable = true;
 
+    enableDefaultConfig = false;
+
     matchBlocks = {
+      "*" = {
+        hashKnownHosts = true;
+	serverAliveInterval = 60;
+	serverAliveCountMax = 3;
+	identitiesOnly = true;
+      };
+
       "github.com" = {
         user = "git";
-	hostname = "github";
+	hostname = "github.com";
 	identityFile = "~/.ssh/id_ed25519";
 	identitiesOnly = true;
       };
@@ -23,6 +32,4 @@
       };
     };
   };
-
-  services.ssh-agent.enable = true;
 }
