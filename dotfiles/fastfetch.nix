0352778febb1e4b5557b7677d7464acccda8386a
nekrofrukt <nekrofrukt.pm.me>
2026-02-07 21:21:55 +0100

    Updated fastfetch config

--- dotfiles/fastfetch.nix
+++ dotfiles/fastfetch.nix
@@ -9,29 +9,26 @@
 		"separator": " "
   	},
 	"modules": [
-		"title",
-		"separator",
+		{"key": "{#36}MU/TH/UR : 180924609{#keys}", "type": "custom"},
+                {"key": "{#36}--------------------{#keys}", "type": "custom"},
+		//"title",
+		//"separator",
 		{"key": "{#31}SYSTEM{#keys}", "type": "custom"},
 		{"key": "OPS", "type": "os"},
 		{"key": "KRN", "type": "kernel"},
-		"separator",
-		//"break",
-
+		"break",
 		{"key": "{#31}SOFTWARE{#keys}", "type": "custom"},
 		{"key": "DSK", "type": "de"},
 		{"key": "PKG", "type": "packages"},
 		{"key": "TER", "type": "terminal"},
 		{"key": "SHL", "type": "shell"},
-		"separator",
-		//"break",
-
+		"break",
 		{"key": "{#31}HARDWARE{#keys}", "type": "custom"},
 		{"key": "HST", "type": "host"},
 		{"key": "CPU", "type": "cpu"},
 		{"key": "RAM", "type": "memory"},
 		{"key": "UPT", "type": "uptime"},
-		"separator",
-		//"break",
+		"break",
 		{"key": "HEX", "type": "colors", "symbol": "circle",}
 	]
     }
