bec196868698b34bd820c6c4721faf3fc7856136
nekrofrukt <nekrofrukt.pm.me>
2026-02-07 12:38:39 +0100

    Edited fastfetch config

--- dotfiles/fastfetch.nix
+++ dotfiles/fastfetch.nix
@@ -10,26 +10,28 @@
   	},
 	"modules": [
 		"title",
-		"break",
-		//"separator",
+		"separator",
 		{"key": "{#31}SYSTEM{#keys}", "type": "custom"},
 		{"key": "OPS", "type": "os"},
 		{"key": "KRN", "type": "kernel"},
-		"break",
+		"separator",
+		//"break",
 
 		{"key": "{#31}SOFTWARE{#keys}", "type": "custom"},
 		{"key": "DSK", "type": "de"},
 		{"key": "PKG", "type": "packages"},
 		{"key": "TER", "type": "terminal"},
 		{"key": "SHL", "type": "shell"},
-		"break",
+		"separator",
+		//"break",
 
 		{"key": "{#31}HARDWARE{#keys}", "type": "custom"},
 		{"key": "HST", "type": "host"},
 		{"key": "CPU", "type": "cpu"},
 		{"key": "RAM", "type": "memory"},
 		{"key": "UPT", "type": "uptime"},
-		"break",
+		"separator",
+		//"break",
 		{"key": "HEX", "type": "colors", "symbol": "circle",}
 	]
     }
