81a897fd0c0e7a4930a0bccd73a8695e88d03838
nekrofrukt <nekrofrukt.pm.me>
2026-02-12 11:02:08 +0100

    Added xps aliases to bash.nix.

--- dotfiles/bash.nix
+++ dotfiles/bash.nix
@@ -14,10 +14,14 @@
       ls = "ls -a";
 
       update_flake = "sudo nix flake update";
+      collect_garbage = "sudo nix-collect-garbage --delete-older-than 7d";
+ 
       rebuild_switch_t14 = "sudo nixos-rebuild switch --flake .#t14";
       rebuild_test_t14 = "sudo nixos-rebuild test --flake .#t14";
-      collect_garbage = "sudo nix-collect-garbage --delete-older-than 7d";
       
+      rebuild_switch_xps = "sudo nixos-rebuild switch --flake .#xps";
+      rebuild_test_xps = "sudo nixos-rebuild test --flake .#xps";
+
       cd_etc = "cd /etc/nixos/";
       cd_nixos = "cd ~/src/nixos";
 
