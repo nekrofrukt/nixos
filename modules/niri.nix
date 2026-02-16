e0b85b78a0dda399125dc05909e01097e12c4cb3
nekrofrukt <nekrofrukt.pm.me>
2026-02-16 22:11:51 +0100

    Edited niri module.

--- modules/niri.nix
+++ modules/niri.nix
@@ -1,15 +1,23 @@
 { config, pkgs, ... }:
 
 {
-  # NIRI WM
+  # NIRI, DM, X11 WINDOWING SYSTEM
   programs.niri.enable = true;
-  
+  services.displayManager.gdm.enable = true;
+  services.xserver.enable = true;
+
   # NIRI SPECIFIC PKGS
   environment.systemPackages = with pkgs; [
+    alacritty
+    fuzzel
     mako
     rofi
     swaybg
-    waybar
     xwayland-satellite
   ];
+
+  # NOCTILIA DEPENDENCIES
+  hardware.bluetooth.enable = true;
+  services.power-profiles-daemon.enable = true;
+  services.upower.enable = true;
 }
