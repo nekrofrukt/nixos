080c2d350d64f66078f2a51cc29534408198f802
nekrofrukt <nekrofrukt.pm.me>
2026-02-17 11:40:44 +0100

    Edited init.lua.

--- dotfiles/nvim/nvim.nix
+++ dotfiles/nvim/nvim.nix
@@ -25,6 +25,11 @@
     initLua = ''
       require("config.lazy")
 
+      vim.opt.tabstop = 4
+      vim.opt.shiftwidth = 4
+      vim.opt.softtabstop = 4
+      vim.opt.expandtab = true
+
       vim.opt.number = true
       vim.opt.relativenumber = true
       vim.opt.clipboard = "unnamedplus"
